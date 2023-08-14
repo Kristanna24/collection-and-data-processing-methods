e24e4a8c2ecd070031be3f935d2f50e7547c4c0c
Kristanna24 <panchokha1990@mail.ru>
2023-08-14 19:59:31 +0300

    Lesson_07

---
+++ Lesson_07/pythonProject8/main.py
@@ -0,0 +1,12 @@
+# терминал:
+# pip install scrapy
+# scrapy startproject название проекта .
+# scrapy genspider название(castoramaru) домен (castorama.ru)
+
+# Чтобы проверить работает ли url
+# scrapy crawl castoramaru
+
+# создаем файл для отладки runner в проекте castorama_p
+
+
+# pip install pymongo
