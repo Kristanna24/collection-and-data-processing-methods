97cc2d63408925b2863781be2b3e045f8e57f227
Kristanna24 <panchokha1990@mail.ru>
2023-08-21 20:28:22 +0300

    Lesson_8

---
+++ Lesson_8/avitosplash08/main.py
@@ -0,0 +1,40 @@
+# pip install scrapy
+
+# https://github.com/scrapy-plugins/scrapy-splash?ysclid=lldmrf3k92988801580
+
+# pip install scrapy-splash
+# scrapy startproject avitoparser .
+# scrapy genspider avito avito.ru
+
+# вставляем в настройки из https://github.com/scrapy-plugins/scrapy-splash?ysclid=lldmrf3k92988801580:
+#
+# SPLASH_URL = 'http://192.168.59.103:8050'
+# SPLASH_URL = 'http://localhost:8050'
+#
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy_splash.SplashCookiesMiddleware': 723,
+#     'scrapy_splash.SplashMiddleware': 725,
+#     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+# }
+#
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+# }
+#
+# DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+# HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+# все коды из файлов middle...
+
+# в паука и функцию:
+# from scrapy_splash import SplashRequest
+
+# with open("cat1.json", 'wb') as f:
+#     f.write(response.body)
+
+# скачать фото
+# pip install pillow
+# setting:
+# IMAGES_STORE = 'photos'
+
+# scrapy runspider avito.py -o avitoparseritem.json
